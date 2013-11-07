d8acdcaf5da46928e912ef8c8724253a0450763c
gabriela <gzunigar@gmail.com>
2013-11-07 17:10:17 -0500

    agrega info de usuarios al chat

--- client/client.js
+++ client/client.js
@@ -2,15 +2,32 @@ Template.messages.messages = function() {
   return Messages.find({}, {sort: {time: -1}});
 }
 
+Template.user.user = function(){
+
+  return Meteor.user().services.twitter.screenName; 
+}
+
+Template.user.imagen = function(){
+
+  return Meteor.user().services.twitter.profile_image_url; 
+}
+
 Template.input.events = {
   'submit #myform' : function (event) {
     event.preventDefault();
-    var name = 'Anonymous';
-    var message = $('#message');
-
+    if (Meteor.user())
+      {  var name = Meteor.user().profile.name;
+        var scrname= Meteor.user().services.twitter.screenName;
+      }
+      else
+      {  var name = 'Anonymous';
+         var scrname=''; 
+      }
+      var message = $('#message');
     if (message.value != '') {
       Messages.insert({
         name: name,
+        account: scrname,
         message: message.val(),
         time: Date.now(),
       });
