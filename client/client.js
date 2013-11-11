81db20bb0da3a98b8b7f63cfd748748efa3a80be
brau <soncco@gmail.com>
2013-11-11 08:00:30 -0500

    Js Format

--- client/client.js
+++ client/client.js
@@ -1,39 +1,39 @@
 Template.messages.messages = function() {
   return Messages.find({}, {sort: {time: -1}});
+  // SELECT * FROM Messages ORDER BY time DESC;
 }
 
 Template.user.user = function(){
-
   return Meteor.user().services.twitter.screenName; 
 }
 
 Template.user.imagen = function(){
-
   return Meteor.user().services.twitter.profile_image_url; 
 }
 
 Template.input.events = {
   'submit #myform' : function (event) {
     event.preventDefault();
-    if (Meteor.user())
-      {  var name = Meteor.user().profile.name;
-        var scrname= Meteor.user().services.twitter.screenName;
-      }
-      else
-      {  var name = 'Anonymous';
-         var scrname=''; 
-      }
-      var message = $('#message');
-    if (message.value != '') {
+
+    if (Meteor.user()) {
+      var name = Meteor.user().profile.name;
+      var scrname = Meteor.user().services.twitter.screenName;
+    } else {
+      var name = 'Anonymous';
+      var scrname = ''; 
+    }
+    
+    var $message = $('#message');
+
+    if ($message.val() != '') {
       Messages.insert({
         name: name,
         account: scrname,
-        message: message.val(),
+        message: $message.val(),
         time: Date.now(),
       });
 
-      document.getElementById('message').value = '';
-      message.value = '';
+      $message.val('');
     }
   }
 }
